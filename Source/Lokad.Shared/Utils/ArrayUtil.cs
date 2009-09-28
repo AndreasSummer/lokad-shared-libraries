ed8397794f2ed161bef6b333350500547d068132
rinat.abdullin@2c947220-0ae5-11de-b17c-0f1ef23b492c <rinat.abdullin@2c947220-0ae5-11de-b17c-0f1ef23b492c>
2009-09-28 20:50:36 +0000

    Shared: cosmetics, exposed ArrayUtil.Empty

--- Source/Lokad.Shared/Utils/ArrayUtil.cs
+++ Source/Lokad.Shared/Utils/ArrayUtil.cs
@@ -11,7 +11,7 @@ using System;
 namespace Lokad
 {
 	/// <summary>
-	/// Utility class to manipulate arrays of arrays
+	/// Utility class to manipulate arrays
 	/// </summary>
 	public static class ArrayUtil
 	{
@@ -28,15 +28,25 @@ namespace Lokad
 		/// <summary>
 		/// Empty array of <see cref="Guid"/>
 		/// </summary>
-		public static readonly Guid[] EmptyGuid = new Guid[0];
+		public static readonly Guid[] EmptyGuid = Empty<Guid>();
 		/// <summary>
 		/// Empty array of <see cref="int"/>
 		/// </summary>
-		public static readonly int[] EmptyInt32 = new int[0];
+		public static readonly int[] EmptyInt32 = Empty<int>();
 		/// <summary>
 		/// Empty array of <see cref="string"/>
 		/// </summary>
-		public static readonly string[] EmptyString = new string[0];
+		public static readonly string[] EmptyString = Empty<string>();
+
+		/// <summary>
+		/// Returns empty array instance
+		/// </summary>
+		/// <typeparam name="T">type of the item for the array</typeparam>
+		/// <returns>empty array singleton</returns>
+		public static T[] Empty<T>()
+		{
+			return ArrayUtil<T>.Empty;
+		}
 	}
 
 	static class ArrayUtil<T>
