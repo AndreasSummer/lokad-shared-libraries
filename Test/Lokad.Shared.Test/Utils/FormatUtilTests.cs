8bbe20ea86df23ed594693cc2eca98e0fb37d359
rinat.abdullin@2c947220-0ae5-11de-b17c-0f1ef23b492c <rinat.abdullin@2c947220-0ae5-11de-b17c-0f1ef23b492c>
2010-05-31 11:11:10 +0000

    Pretty format routines for dates and sizes (from Lokad.Cloud and CloudBus).

---
+++ Test/Lokad.Shared.Test/Utils/FormatUtilTests.cs
@@ -0,0 +1,32 @@
+﻿#region (c)2009-2010 Lokad - New BSD license
+
+// Copyright (c) Lokad 2009-2010 
+// Company: http://www.lokad.com
+// This code is released under the terms of the new BSD licence
+
+#endregion
+
+using System;
+using NUnit.Framework;
+
+namespace Lokad
+{
+	[TestFixture]
+	public sealed class FormatUtilTests
+	{
+		// ReSharper disable InconsistentNaming
+
+		[Test]
+		public void Formatting_large_interval()
+		{
+			StringAssert.Contains("years from now", FormatUtil.TimeOffsetUtc(DateTime.MaxValue));
+			StringAssert.Contains("years ago", FormatUtil.TimeOffsetUtc(DateTime.MinValue));
+		}
+
+		[Test]
+		public void Formatting_large_size()
+		{
+			Assert.AreEqual("8 EB", FormatUtil.SizeInBytes(long.MaxValue));
+		}
+	}
+}
