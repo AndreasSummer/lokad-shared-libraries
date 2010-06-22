b246a25eb76189413484f1fd040b139e141dbdb1
rinat.abdullin@gmail.com@2c947220-0ae5-11de-b17c-0f1ef23b492c <rinat.abdullin@gmail.com@2c947220-0ae5-11de-b17c-0f1ef23b492c>
2010-06-22 14:51:25 +0000

    Shared: tests for CombGuid (against SQL Server guid comparer)

---
+++ Test/Lokad.Shared.Test/Utils/GuidUtilTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Lokad.Data.SqlClient;
+using NUnit.Framework;
+
+namespace Lokad
+{
+	[TestFixture]
+	public sealed class GuidUtilTests
+	{
+		// ReSharper disable InconsistentNaming
+
+		[Test]
+		public void Combs_are_SQL_Ordered()
+		{
+			var initial = Range.Array(50, n =>
+				{
+					SystemUtil.Sleep(20.Milliseconds());
+					return GuidUtil.NewComb();
+				});
+
+			var copy = new List<Guid>(initial);
+			copy.Sort(new SqlServerGuidComparer());
+			CollectionAssert.AreEqual(initial, copy);
+		}
+	}
+}
