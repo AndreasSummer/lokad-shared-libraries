33212b1e216352b24d5af5bedc7452045c671ff8
rinat.abdullin@2c947220-0ae5-11de-b17c-0f1ef23b492c <rinat.abdullin@2c947220-0ae5-11de-b17c-0f1ef23b492c>
2010-07-21 15:21:09 +0000

    Shared: moved CQRS interfaces to separate project, that gets merged with Lokad.Shared.dll.

---
+++ Source/Lokad.Cqrs/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿#region (c)2009 Lokad - New BSD license
+
+// Copyright (c) Lokad 2009 
+// Company: http://www.lokad.com
+// This code is released under the terms of the new BSD licence
+
+#endregion
+
+using System;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Lokad.CQRS")]
+[assembly: AssemblyDescription("Lokad CQRS interfaces")]
+[assembly: AssemblyCopyright(GlobalAssemblyInfo.Copyright)]
+[assembly: AssemblyTrademark(GlobalAssemblyInfo.Trademark)]
+[assembly: CLSCompliant(false)]
